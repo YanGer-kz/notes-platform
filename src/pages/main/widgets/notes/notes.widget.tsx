9c3c84c0df40293932dc52f4cc9c8f28b8dcbb6a
Ilyas <weendibol21300@gmail.com>
2025-10-10 20:33:19 +0500

    Add NotesWidget component with styles and index export; integrate Tailwind CSS for consistent design

---
+++ src/pages/main/widgets/notes/notes.widget.tsx
@@ -0,0 +1,12 @@
+import './notes.style.css'
+
+import { defineComponent } from 'vue'
+
+export default defineComponent(() => {
+  return () => (
+    <>
+      <div class="widget__notes">
+      </div>
+    </>
+  )
+})
