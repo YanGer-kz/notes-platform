87b190e94fd880be9850810abfad631ac5f186e9
Ilyas <weendibol21300@gmail.com>
2025-10-11 08:11:15 +0500

    Add navigation structure and styles to NotesWidget component; implement grid layout and item styling using Tailwind CSS

--- src/pages/main/widgets/notes/notes.widget.tsx
+++ src/pages/main/widgets/notes/notes.widget.tsx
@@ -6,6 +6,38 @@ export default defineComponent(() => {
   return () => (
     <>
       <div class="widget__notes">
+        <ul class="navigation">
+          <li class="item active">
+            <span class="date">
+              THU, 9 OCT
+            </span>
+
+            <h1 class="label">
+              Lorem, ipsum dolor.
+            </h1>
+
+            <p class="description">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. At recusandae modi suscipit sed earum esse adipisci beatae nam ...
+            </p>
+          </li>
+
+          <li class="item">
+            <span class="date">
+              SAT, 11 OCT
+            </span>
+
+            <h1 class="label">
+              Lorem, ipsum dolor.
+            </h1>
+
+            <p class="description">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. At recusandae modi suscipit sed earum esse adipisci beatae nam ...
+            </p>
+          </li>
+        </ul>
+
+        <div class="content">
+        </div>
       </div>
     </>
   )
