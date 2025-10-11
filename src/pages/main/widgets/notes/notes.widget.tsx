218b54b61bf28a38839ce4c509b25655e948030a
Ilyas <weendibol21300@gmail.com>
2025-10-11 20:31:16 +0500

    Refactor NotesWidget layout and styles; increase gap between grid items and add header section for note details using Tailwind CSS

--- src/pages/main/widgets/notes/notes.widget.tsx
+++ src/pages/main/widgets/notes/notes.widget.tsx
@@ -47,6 +47,20 @@ export default defineComponent(() => {
         </ul>
 
         <div class="content">
+          <div class="header">
+            <span class="date">
+              SAT, 11 OCT
+            </span>
+
+            <h1 class="label">
+              Lorem, ipsum dolor.
+            </h1>
+          </div>
+
+          <p class="text">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione animi tempora soluta officia? Enim architecto blanditiis cumque quibusdam eum veniam provident facere, eos modi magnam dolores dolore dolorem, mollitia molestiae!
+            Sequi nobis totam amet cupiditate soluta, quidem corporis quod asperiores perferendis neque adipisci laboriosam nisi molestiae sed quibusdam odit sint perspiciatis itaque dicta similique inventore ipsam, error vel ducimus! In?
+          </p>
         </div>
       </div>
     </>
