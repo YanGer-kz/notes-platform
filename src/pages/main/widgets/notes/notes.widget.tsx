454cde5b28ae69f3f53969bed01e43e0e345ea77
Ilyas <weendibol21300@gmail.com>
2025-10-11 08:22:24 +0500

    Add 'Add new Note' button to NotesWidget with corresponding styles; enhance navigation layout and interactivity using Tailwind CSS

--- src/pages/main/widgets/notes/notes.widget.tsx
+++ src/pages/main/widgets/notes/notes.widget.tsx
@@ -2,11 +2,21 @@ import './notes.style.css'
 
 import { defineComponent } from 'vue'
 
+import { UIIcon } from '@/shared/ui/icon'
+
 export default defineComponent(() => {
   return () => (
     <>
       <div class="widget__notes">
         <ul class="navigation">
+          <li class="item add">
+            <UIIcon icon="plus"/>
+
+            <h1 class="label">
+              Add new Note
+            </h1>
+          </li>
+
           <li class="item active">
             <span class="date">
               THU, 9 OCT
