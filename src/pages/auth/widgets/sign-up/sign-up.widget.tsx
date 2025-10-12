fe48dc657c7fd34f046bf28284e825d8d4159535
Ilyas <weendibol21300@gmail.com>
2025-10-12 23:29:37 +0500

    Add SignUpWidget component and associated styles for user registration functionality.

---
+++ src/pages/auth/widgets/sign-up/sign-up.widget.tsx
@@ -0,0 +1,12 @@
+import './sign-up.style.css'
+
+import { defineComponent } from 'vue'
+
+export default defineComponent(() => {
+  return () => (
+    <>
+      <div class="widget__sign_up">
+      </div>
+    </>
+  )
+})
