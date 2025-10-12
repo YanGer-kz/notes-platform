12712b3004fc667e5dd28cde0e915c399342b3b1
Ilyas <weendibol21300@gmail.com>
2025-10-12 21:36:36 +0500

    Add authentication page and routing; integrate AuthPageRoute into the main router and create initial auth page component with basic styling.

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import AuthPageRoute from '@/pages/auth/auth.route'
 import MainPageRoute from '@/pages/main/main.route'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    ...AuthPageRoute,
     ...MainPageRoute,
   ],
 })
---
+++ src/pages/auth/auth.page.tsx
@@ -0,0 +1,11 @@
+import { defineComponent } from 'vue'
+
+export default defineComponent(() => {
+  return () => (
+    <>
+      <div class="page-auth">
+        
+      </div>
+    </>
+  )
+})
