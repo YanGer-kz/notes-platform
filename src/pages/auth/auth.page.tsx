5ef4e46a82fc37b8e1603b3e21902b96393f5936
Ilyas <weendibol21300@gmail.com>
2025-10-12 23:48:13 +0500

    Implement dynamic routing for authentication page to toggle between SignInWidget and SignUpWidget based on route; add redirection for default auth path.

--- src/pages/auth/auth.page.tsx
+++ src/pages/auth/auth.page.tsx
@@ -1,14 +1,24 @@
 import './auth.style.css'
 
 import { defineComponent } from 'vue'
+import { useRoute } from 'vue-router'
 
 import { SignInWidget } from './widgets/sign-in'
+import { SignUpWidget } from './widgets/sign-up'
 
 export default defineComponent(() => {
+  const route = useRoute()
+
   return () => (
     <>
       <div class="page__auth">
-        <SignInWidget/>
+        {
+          route.path === '/auth/sign-in' ? (
+            <SignInWidget/>
+          ) : (
+            <SignUpWidget/>
+          )
+        }
       </div>
     </>
   )
--- src/pages/auth/auth.route.ts
+++ src/pages/auth/auth.route.ts
@@ -2,7 +2,14 @@ import type { RouteRecordRaw } from 'vue-router'
 
 export default <RouteRecordRaw[]> [
   {
-    path: '/auth',
+    path: '/auth/:type?',
     component: () => import('./auth.page'),
+    beforeEnter: (to, _, next) => {
+      if (to.path === '/auth') {
+        return next({ path: '/auth/sign-in' })
+      }
+
+      return next()
+    }
   }
 ]
