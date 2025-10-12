dfd25f095d854cd6cd461663caef6978ed1419d5
Ilyas <weendibol21300@gmail.com>
2025-10-12 21:17:39 +0500

    Refactor NotesWidget to integrate notes store for dynamic note management; implement note retrieval and editing functionality, and enhance date formatting for better display.

--- src/pages/main/widgets/notes/notes.widget.tsx
+++ src/pages/main/widgets/notes/notes.widget.tsx
@@ -1,66 +1,95 @@
 import './notes.style.css'
 
-import { defineComponent } from 'vue'
+import { defineComponent, ref, type Ref } from 'vue'
+
+import { useNotesStore } from '@/app/stores'
 
 import { UIButton } from '@/shared/ui/button'
 import { UIInput } from '@/shared/ui/input'
 import { UITextarea } from '@/shared/ui/textarea'
 
 export default defineComponent(() => {
-  return () => (
-    <>
-      <div class="widget__notes">
-        <ul class="navigation">
-          <UIButton label='Add new Note' icon='plus' severity='secondary' size='large'/>
+  const notesStore = useNotesStore()
 
-          <li class="item active">
-            <span class="date">
-              THU, 9 OCT
-            </span>
+  const noteId: Ref<number | null> = ref(null)
 
-            <h1 class="label">
-              Lorem, ipsum dolor.
-            </h1>
+  const formatDate = (date: Date) => {
+    const months: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    const weeks: string[] = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
-            <p class="description">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. At recusandae modi suscipit sed earum esse adipisci beatae nam ...
-            </p>
-          </li>
+    return [weeks[(date.getDay() || 7) - 1]?.toUpperCase(), [date.getDate(), months[date.getMonth()]?.toUpperCase()].join(' ')].join(', ')
+  }
 
-          <li class="item">
-            <span class="date">
-              SAT, 11 OCT
-            </span>
+  const getNotes = () => {
+    return notesStore.getNotes.map(item => (
+      <li class={["item", { active: noteId.value === item.id }]} onClick={() => noteId.value = item.id}>
+        <span class="date">
+          {
+            formatDate(item.created_at) // THU, 9 OCT
+          }
+        </span>
 
-            <h1 class="label">
-              Lorem, ipsum dolor.
-            </h1>
+        <h1 class="label">
+          {
+            item.label
+          }
+        </h1>
 
-            <p class="description">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. At recusandae modi suscipit sed earum esse adipisci beatae nam ...
-            </p>
-          </li>
-        </ul>
+        <p class="description">
+          {
+            item.text.length > 140 ? [item.text.slice(0, 140), '...'].join(' ') : item.text
+          }
+        </p>
+      </li>
+    ))
+  }
 
+  const getNote = () => {
+    const note = notesStore.getNote(noteId.value)
+
+    if (note) {
+      return (
         <div class="content">
           <div class="header">
             <div class="group">
               <span class="date">
-                SAT, 9 OCT
+                {
+                  formatDate(note.created_at) // THU, 9 OCT
+                }
               </span>
-
+  
               <h1 class="label">
-                <UIInput value='Lorem, ipsum dolor.' fulid onValue={val => console.log(val)}/>
+                <UIInput value={note.label} fulid onValue={val => note.label = val}/>
               </h1>
             </div>
-
+  
             <UIButton icon='trash-can-outline' severity='danger'/>
           </div>
-
+  
           <p class="text">
-            <UITextarea value='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione animi tempora soluta officia? Enim architecto blanditiis cumque quibusdam eum veniam provident facere, eos modi magnam dolores dolore dolorem, mollitia molestiae! Sequi nobis totam amet cupiditate soluta, quidem corporis quod asperiores perferendis neque adipisci laboriosam nisi molestiae sed quibusdam odit sint perspiciatis itaque dicta similique inventore ipsam, error vel ducimus! In?' fulid onValue={val => console.log(val)}/>
+            <UITextarea value={note.text} fulid onValue={val => note.text = val}/>
           </p>
         </div>
+      )
+    }
+
+    return null
+  }
+
+  return () => (
+    <>
+      <div class="widget__notes">
+        <ul class="navigation">
+          <UIButton label='Add new Note' icon='plus' severity='secondary' size='large' onClick={() => notesStore.addNote()}/>
+
+          {
+            getNotes()
+          }
+        </ul>
+
+        {
+          getNote()
+        }
       </div>
     </>
   )
