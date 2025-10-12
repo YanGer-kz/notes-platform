ab8a9121b6c25f193d81552bf1868b075d0f86a6
Ilyas <weendibol21300@gmail.com>
2025-10-12 23:48:00 +0500

    Enhance SignUpWidget by adding structured header and form sections; implement responsive styles for improved user experience.

--- src/pages/auth/widgets/sign-up/sign-up.widget.tsx
+++ src/pages/auth/widgets/sign-up/sign-up.widget.tsx
@@ -1,11 +1,36 @@
 import './sign-up.style.css'
 
 import { defineComponent } from 'vue'
+import { useRouter } from 'vue-router'
+
+import { UIInput } from '@/shared/ui/input'
+import { UIButton } from '@/shared/ui/button'
 
 export default defineComponent(() => {
+  const router = useRouter()
+
   return () => (
     <>
       <div class="widget__sign_up">
+        <div class="header">
+          <h1 class="label">
+            Sign Up
+          </h1>
+
+          <p class="text">
+            Create a new account
+          </p>
+        </div>
+
+        <div class="form">
+          <UIInput value='' placeholder='Enter login' variant='outlined'/>
+          <UIInput value='' placeholder='Enter name' variant='outlined'/>
+          <UIInput type='password' value='' placeholder='Enter password' variant='outlined'/>
+          <UIInput type='password' value='' placeholder='Confirm password' variant='outlined'/>
+        </div>
+
+        <UIButton label='Sign Up'/>
+        <UIButton label='There is an account' severity='secondary' onClick={() => router.push({ path: '/auth/sign-in' })}/>
       </div>
     </>
   )
