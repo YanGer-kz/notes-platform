cfa0d10b667b01504c32a007aa92f58e2470c806
Ilyas <weendibol21300@gmail.com>
2025-10-12 22:54:22 +0500

    Add SignInWidget component and associated styles; create initial structure for sign-in functionality.

---
+++ src/pages/auth/widgets/sign-in/sign-in.widget.tsx
@@ -0,0 +1,13 @@
+import './sign-in.style.css'
+
+import { defineComponent } from 'vue'
+
+export default defineComponent(() => {
+  return () => (
+    <>
+      <div class="widget__sign_in">
+        
+      </div>
+    </>
+  )
+})
