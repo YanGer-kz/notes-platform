d15f3023f18c248c229fbd3c77e458397f12572d
Ilyas <weendibol21300@gmail.com>
2025-10-12 23:20:57 +0500

    Add structured layout to SignInWidget with header and form sections; enhance styles for improved user experience.

--- src/pages/auth/widgets/sign-in/sign-in.widget.tsx
+++ src/pages/auth/widgets/sign-in/sign-in.widget.tsx
@@ -2,11 +2,26 @@ import './sign-in.style.css'
 
 import { defineComponent } from 'vue'
 
+import { UIInput } from '@/shared/ui/input'
+
 export default defineComponent(() => {
   return () => (
     <>
       <div class="widget__sign_in">
-        
+        <div class="header">
+          <h1 class="label">
+            Sign In
+          </h1>
+
+          <p class="text">
+            Welcome to Notes Platform
+          </p>
+        </div>
+
+        <div class="form">
+          <UIInput value='' placeholder='Enter login' variant='outlined'/>
+          <UIInput value='' placeholder='Enter password' variant='outlined'/>
+        </div>
       </div>
     </>
   )
