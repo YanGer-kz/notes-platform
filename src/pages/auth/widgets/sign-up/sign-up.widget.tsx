0bef475d3b433d361b83b84c61460f7538bf439a
Ilyas <weendibol21300@gmail.com>
2025-10-13 09:54:22 +0500

    Add validation and error handling in SignUpWidget; display error messages for empty fields and mismatched passwords.

--- src/pages/auth/widgets/sign-up/sign-up.widget.tsx
+++ src/pages/auth/widgets/sign-up/sign-up.widget.tsx
@@ -1,6 +1,6 @@
 import './sign-up.style.css'
 
-import { defineComponent } from 'vue'
+import { defineComponent, ref, type Ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 import { UIInput } from '@/shared/ui/input'
@@ -8,6 +8,23 @@ import { UIButton } from '@/shared/ui/button'
 
 export default defineComponent(() => {
   const router = useRouter()
+
+  const alerts: Ref<string[]> = ref([])
+
+  const login: Ref<string> = ref('')
+  const name: Ref<string> = ref('')
+  const password: Ref<string> = ref('')
+  const confirm_password: Ref<string> = ref('')
+
+  const handleSubmit = () => {
+    if (!login.value || !name.value || !password.value || !confirm_password.value) {
+      return alerts.value.push('Login / name / password or confirm password is empty')
+    }
+
+    if (password.value !== confirm_password.value) {
+      return alerts.value.push('password and confirm password not equals')
+    }
+  }
 
   return () => (
     <>
@@ -22,14 +39,24 @@ export default defineComponent(() => {
           </p>
         </div>
 
+        {
+          alerts.value.map((item, i) => (
+            <span key={i} class="error-message">
+              {
+                item
+              }
+            </span>
+          ))
+        }
+
         <div class="form">
-          <UIInput value='' placeholder='Enter login' variant='outlined'/>
-          <UIInput value='' placeholder='Enter name' variant='outlined'/>
-          <UIInput type='password' value='' placeholder='Enter password' variant='outlined'/>
-          <UIInput type='password' value='' placeholder='Confirm password' variant='outlined'/>
+          <UIInput value={login.value} placeholder='Enter login' variant='outlined' onValue={val => login.value = val}/>
+          <UIInput value={name.value} placeholder='Enter name' variant='outlined' onValue={val => name.value = val}/>
+          <UIInput type='password' value={password.value} placeholder='Enter password' variant='outlined' onValue={val => password.value = val}/>
+          <UIInput type='password' value={confirm_password.value} placeholder='Confirm password' variant='outlined' onValue={val => confirm_password.value = val}/>
         </div>
 
-        <UIButton label='Sign Up'/>
+        <UIButton label='Sign Up' onClick={handleSubmit}/>
         <UIButton label='There is an account' severity='secondary' onClick={() => router.push({ path: '/auth/sign-in' })}/>
       </div>
     </>
